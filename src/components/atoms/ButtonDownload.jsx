32ae0bcec43bcad8b142ae523c382609aca3554a
M. Hanif Irfan <mhirfan173@gmail.com>
2026-04-06 09:54:55 +0700

    feat: add atoms components

---
+++ src/components/atoms/ButtonDownload.jsx
@@ -0,0 +1,34 @@
+/**
+ * 
+ * @param {Object} props
+ * @param {string} props.icon // ganti src gambar
+ * @param {string} props.text // masukkan teks
+ * @param {string} props.altText // memberikan alt-text dari Image
+ * @param {string} props.imgSize // edit ukuran gambar
+ * @param {string} props.invertOnHover // memberikan efek inver saat hover agar terbaca
+ * @param {string} props.variant // memberikan variant style button
+ * @returns {JSX.Element}
+ */
+
+function ButtonDownload({icon, text, altText,imgSize, invertOnHover,variant}){
+    const colorVariant= {
+        primary: 'bg-blue-600 text-white border-blue-600 hover:bg-blue-700',
+        secondary: 'bg-white text-blue-600 border-white hover:bg-gray-100',
+        outline: 'bg-transparant border-white text-white hover:bg-white hover:text-blue-600',
+        outlineBlue: 'bg-transparant border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white hover:border-white'
+    }
+
+    return (
+        <button className= {`group border flex justify-center items-center gap-4 flex-1 p-2  rounded-md cursor-pointer ${colorVariant[variant]}`}>
+            <img 
+            src={icon} 
+            alt={altText} 
+            className={`${imgSize} ${invertOnHover ? "group-hover:invert" : ""}`}
+            />
+
+            <p className="font-montserrat font-light">{text}</p>
+        </button>      
+    )
+}
+
+export default ButtonDownload
---
+++ src/components/atoms/Logo.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router";
+
+
+/**
+ * 
+ * @param {Object} props
+ * @param {String} props.textColor //ubah warna text
+ * @param {String} props.className //kustomisasi style
+ * @returns {JSX.Element}
+ */
+function Logo ({textColor, className = ""}){
+    return(
+        <Link to="/" className={`flex items-center cursor-pointer gap-2 ${className}`}>
+        <img
+          src="../src/assets/money_wallet.png"
+          alt="icon-logo"
+          className="w-8"
+        />
+        <p className={`font-nunito-sans ${textColor} font-medium`}>E-Wallet</p>
+      </Link>
+
+    )
+}
+
+export default Logo

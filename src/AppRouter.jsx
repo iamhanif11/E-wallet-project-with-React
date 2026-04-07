414d9198ab18cfd502808918c7c1d580f9dd963b
M. Hanif Irfan <mhirfan173@gmail.com>
2026-04-08 03:21:54 +0700

    fix: delete file

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -3,7 +3,6 @@ import LandingPage from "./pages/LandingPage"
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import CreatePin from "./pages/CreatePin";
 import TransferPage from "./pages/TransferPage";
 import TransferDetail from "./pages/TransferDetail";
 
@@ -13,7 +12,6 @@ function AppRouter() {
             <Route path="/" element={<LandingPage/>}/>
             <Route path="/Login" element={<Login/>}/>
             <Route path="/Register" element={<Register/>}/>
-            <Route path="/CreatePin" element={<CreatePin/>}/>
             <Route path= "/Dashboard" element={<Dashboard/>}/>
             <Route path= "/Transfer" element={<TransferPage/>}/>
             <Route path= "/Detail" element={<TransferDetail/>}></Route>

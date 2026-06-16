c10c4fb0a87f1de73d4aeebdacd5af880818e81f
M. Hanif Irfan <mhirfan173@gmail.com>
2026-06-16 21:37:05 +0700

    fix:editprofile

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -105,7 +105,7 @@ function EditProfile() {
 
     if (imagePath.startsWith("http://") || imagePath.startsWith("https://")) return imagePath;
 
-    const BACKEND_URL = "http://localhost:8080";
+    const BACKEND_URL = "/api";
     const cleanPath = imagePath.startsWith("/") ? imagePath.slice(1) : imagePath
 
     if (cleanPath.startsWith("img/profile")){
